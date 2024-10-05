18ce6202a2eb5cb50cabdcd00eb2338edc97d577
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-10-05 23:35:50 +0700

    first setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// connect to mongodb
+mongoose
+  .connect('mongodb://127.0.0.1/shop_db')
+  .then((res) => {
+    console.log('connected to mongodb');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(3000, () => {
+  console.log('shop app listening on http://127.0.0.1:3000');
+});
